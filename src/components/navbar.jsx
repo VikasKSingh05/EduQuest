8e0ad0a39737fc09ca4e16353a7cffd9131ab631
Satyam-1o <satyam312004@gmail.com>
2025-09-07 23:08:19 +0530

    commit

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Search from "./search";
 
 function Navbar() {
   return (
@@ -13,25 +14,7 @@ function Navbar() {
       </div>
       <div className="navbar-end">
         {/* Search */}
-        <label className="input bg-[#020617] w-24 md:w-auto mr-5">
-          <svg
-            className="h-[1em] opacity-50"
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 24 24"
-          >
-            <g
-              strokeLinejoin="round"
-              strokeLinecap="round"
-              strokeWidth="2.5"
-              fill="none"
-              stroke="currentColor"
-            >
-              <circle cx="11" cy="11" r="8"></circle>
-              <path d="m21 21-4.3-4.3"></path>
-            </g>
-          </svg>
-          <input type="search" required placeholder="Search" />
-        </label>
+        <Search />
         {/* links */}
         <div>
           {/* Web buttons: hidden on small screens, visible on medium+ */}
